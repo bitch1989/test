1f1f71af3efe74be50864660013dcf39a4ef3891
damnit1989 <damnit1989@163.com>
2013-07-23 21:15:50 +0800

    commit dir

---
+++ dir/test/lmm.php
@@ -0,0 +1,7 @@
+<?php
+    for($currencyRow = 2;$currencyRow < $Num;$currencyRow++>){
+        foreach(range('a','z') as $Key => $value){
+            $array[] = $currentyROe->getValue();
+        }
+    }
+
